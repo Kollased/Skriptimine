d34dea8dd52d3d3a99ebe352c12e1809e0e3e2e0
rasmus rahmonen <rasmus.rahmonen@voco.ee>
2024-11-01 05:39:14 -0400

    vaatab selle kataloogi sisu ja iga asja kohta väljastab info - mis asi see on ja loeb need koku, mitu faili, kataloogi ja linki

---
+++ praks7a/list.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo -n "Sisesta kataloogi asukoht/nimi: "
+read directory
+
+if [ ! -d $directory ]; then
+	echo "Viga: $directory nimelist kataloogi ei leitud!"
+	exit 1
+
+cd $directory
+
+fi
+file_count=0
+dir_count=0
+link_count=0
+for item in *; do
+    if [ -f "$item" ]; then
+        echo "$item: fail"
+        ((file_count++))
+    elif [ -d "$item" ]; then
+        echo "$item: kataloog"
+        ((dir_count++))
+    elif [ -l "$item" ]; then
+        echo "$item: link"
+        ((link_count++))
+    fi
+done
+echo
+echo "Selles kataloogis on $file_count failid, $dir_count kataloogid ja $link_count linki."
